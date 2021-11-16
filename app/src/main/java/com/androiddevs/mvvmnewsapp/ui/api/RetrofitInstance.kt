0a4bfc7f69ae38bbc981414c68d28b80be5a83f6
codeessed <gbohunmimakinde@gmail.com>
2021-11-16 21:11:28 +0100

    room and database setup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/RetrofitInstance.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/RetrofitInstance.kt
@@ -1,4 +1,29 @@
 package com.androiddevs.mvvmnewsapp.ui.api
 
+import com.androiddevs.mvvmnewsapp.ui.util.Constants.Companion.BASE_URL
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitInstance {
+    companion object{
+        //retrofit instance
+        private val retrofit by lazy{
+            val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder().addInterceptor(logger).build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        //api instance
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -1,4 +1,8 @@
 package com.androiddevs.mvvmnewsapp.ui.util
 
 class Constants {
+    companion object{
+        const val API_KEY = "84e361436d064bdd99a863731d91ce9c"
+        const val BASE_URL = "https://newsapi.org/"
+    }
 }
